1126870f0ec90e982e6be78df21e7bdfd7794fcd
javierbosch <38163512+javierbosch@users.noreply.github.com>
2018-04-09 20:56:05 +0200

    Add files via upload

---
+++ Board.ino
@@ -0,0 +1,42 @@
+#include <Servo.h>
+
+Servo servo;
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial); //Wait the serial port to connect
+}
+
+void loop() {
+  if (Serial.available())
+  {
+    int v = Serial.parseInt();
+    if (v>=2000 && v<=7180) //The only valids values
+    {
+      int n = v % 1000; //Get the last 3 digits
+      int s = (v - n)/1000; //Get the first digit
+      
+      Serial.println(v);
+      Serial.print("Servo ");
+      Serial.println(s);
+
+      servo.attach(s);
+      
+      if (n>=0 && n<=180){
+        servo.write(n);
+        Serial.print(n);
+        Serial.println(" degrees");
+        }
+      else{
+        Serial.println("Degress not in an acceptable range");          
+        }
+    }
+    else
+    {
+      Serial.println("Not a valid number");
+    }
+    Serial.println("---");
+  }
+  delay(100);
+}
